220f1aacd66acb59d3f7c6d22ead7be4ac32cdf9
sajenno1 <13799597982@qq.com>
2019-12-21 23:08:26 +0800

    none

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# deploy.sh
+# 错误时停止
+set -e
+
+# 打包
+npm run build
+
+# 进入目标文件夹
+cd dist
+
+# 提交到本地仓库
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 提交到 https://github.com:sajenno1/demo-jdmall-mobile.git 项目的 gh-pages 分支
+
+git push -f git@github.com:sajenno1/demo-jdmall-mobile.git master:gh-pages
+
+cd -
+
+#git bash: bash deploy.sh
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
+// vue.config.js
 module.exports = {
-    // 基本路径
-    publicPath: '/',
+    // 基本路径 
+    //比如我的项目地址是 https://github.com/sajenno1/demo-jdmall-mobile
+
+    //我要部署到 https://nusr.github.io/sajenno1/demo-jdmall-mobile,那么 publicPath 要配置为 /sajenno1/demo-jdmall-mobile
+    //publicPath: "/demo-jdmall-mobile",
+    publicPath: process.env.NODE_ENV === "production" ? "/demo-jdmall-mobile" : "/",
     // 输出文件目录
     outputDir: 'dist',
 }
